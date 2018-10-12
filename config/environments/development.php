c7e01813aaa586b9babd44c48c436f00e5a25ae2
Ole Thomas Tallerås <olethomas@dekode.no>
2018-10-12 12:36:11 +0200

    ini_set( 'display_errors',  0 ); -> expects parameter 2 to be string. Use: '0'.

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,5 +1,12 @@
 <?php
-/** Development */
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
-define('SCRIPT_DEBUG', true);
+/**
+ * Development Environment
+ *
+ * @package Teft
+ */
+
+declare( strict_types = 1 );
+
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,7 +1,17 @@
 <?php
-/** Staging */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-/** Disable all file modifications including updates and update notifications */
-define('DISALLOW_FILE_MODS', true);
+/**
+ * Staging Environment
+ *
+ * @package Teft
+ */
+
+declare( strict_types = 1 );
+
+// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_ini_set
+ini_set( 'display_errors', '0' );
+
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
+
+// Disable all file modifications including updates and update notifications.
+define( 'DISALLOW_FILE_MODS', true );
