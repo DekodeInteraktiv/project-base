72475e6ab4a6130389abac1c3b67e96b03f5e3f7
Ole Thomas Tallerås <olethomas@dekode.no>
2018-10-12 13:31:00 +0200

    Strict errors in environment config (#15)

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,7 +1,17 @@
 <?php
-/** Production */
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-/** Disable all file modifications including updates and update notifications */
-define('DISALLOW_FILE_MODS', true);
+/**
+ * Production Environment
+ *
+ * @package Teft
+ */
+
+declare( strict_types = 1 );
+
+// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_ini_set
+ini_set( 'display_errors', '0' );
+
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
+
+// Disable all file modifications including updates and update notifications.
+define( 'DISALLOW_FILE_MODS', true );
