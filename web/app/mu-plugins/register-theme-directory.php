8c2df4756907f748bcd1c41e58fb29f7f1c0db4c
Stian <stian@dekode.no>
2019-11-12 11:57:27 +0100

    Lint code

--- web/app/mu-plugins/register-theme-directory.php
+++ web/app/mu-plugins/register-theme-directory.php
@@ -1,14 +1,18 @@
 <?php
-/*
-Plugin Name:  Register Theme Directory
-Plugin URI:   https://roots.io/bedrock/
-Description:  Register default theme directory
-Version:      1.0.0
-Author:       Roots
-Author URI:   https://roots.io/
-License:      MIT License
-*/
+/**
+ * Plugin Name:  Register Theme Directory
+ * Plugin URI:   https://roots.io/bedrock/
+ * Description:  Register default theme directory
+ * Version:      1.0.0
+ * Author:       Roots
+ * Author URI:   https://roots.io/
+ * License:      MIT License
+ *
+ * @package Teft
+ */
 
-if (!defined('WP_DEFAULT_THEME')) {
-    register_theme_directory(ABSPATH . 'wp-content/themes');
+declare( strict_types = 1 );
+
+if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
+	register_theme_directory( ABSPATH . 'wp-content/themes' );
 }
