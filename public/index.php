296ff0fd2aa46b66d0059b4f3a5e0a6fa42e6436
pederan <peder1976@gmail.com>
2020-10-22 10:47:25 +0200

    added required wp files

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Bootstrapper
+ *
+ * @package Dekode
+ */
+
+declare( strict_types = 1 );
+
+define( 'WP_USE_THEMES', true );
+require __DIR__ . '/wp/wp-blog-header.php';
---
+++ public/wp-config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Config
+ *
+ * @package Dekode
+ */
+
+declare( strict_types = 1 );
+
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+require_once dirname( __DIR__ ) . '/config/application.php';
+require_once ABSPATH . 'wp-settings.php';
