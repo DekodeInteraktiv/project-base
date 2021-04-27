5afe799e7fa20fdf008a26f67d8b7f911b37f1fc
Petter Walbø Johnsgård <petter@dekode.no>
2021-04-27 21:20:39 +0200

    Update wp-scripts and add cssnano to minimize css in production
    Closes #204
    Closes #196

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,16 @@
  * External dependencies
  */
 const plugins = require( '@teft/postcss-preset' );
+const cssnano = require( 'cssnano' );
 
-module.exports = () => ( { plugins } );
+module.exports = ( ctx ) => {
+	const config = {
+		plugins,
+	};
+
+	if ( ctx.env === 'production' ) {
+		config.plugins.push( cssnano() );
+	}
+
+	return config;
+};
