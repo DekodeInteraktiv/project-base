e678baf65d4d044fb4795406a8ffdd83a22621c7
Ole Thomas Tallerås <olethomas@dekode.no>
2021-07-07 14:37:42 +0200

    Avoid customer specific info in codeship deploy (#238)

--- tools/codeship/prod-deploy.sh
+++ tools/codeship/prod-deploy.sh
@@ -3,4 +3,4 @@
 cd ~/clone
 rm -rf tools
 
-#rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ prod_xxxx@bolt61.servebolt.com:/kunder/dekode_3423/prod_xxxx/dekode-deploy
+#rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ customer_name@x.prod.dekodes.no:/customer_name/destination
--- tools/codeship/stage-deploy.sh
+++ tools/codeship/stage-deploy.sh
@@ -6,5 +6,5 @@ cp ~/clone/tools/codeship/post-deploy.sh post-deploy.sh
 
 rm -rf tools
 
-rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ stagen_xxxx@bolt61.servebolt.com:/kunder/dekode_3423/stagen_xxxx/dekode-deploy
-ssh stagen_xxxx@bolt61.servebolt.com 'bash -s' < ~/clone/post-deploy.sh
+rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ customer_name@x.dev05.dekodes.no:/customer_name/destination
+ssh customer_name@x.dev05.dekodes.no 'bash -s' < ~/clone/post-deploy.sh
