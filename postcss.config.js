6dd34468888e46e96c72ca6270e56b826aa78d3e
Petter Walbø Johnsgård <petter@dekode.no>
2021-07-08 10:57:31 +0200

    Update PostCSS packages
    Fixses #239

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,24 @@
 /**
  * External dependencies
  */
-const plugins = require( '@teft/postcss-preset' );
+const atImport = require( 'postcss-import' );
+const autoprefixer = require( 'autoprefixer' );
 const cssnano = require( 'cssnano' );
+const customMedia = require( 'postcss-custom-media' );
+const minmax = require( 'postcss-media-minmax' );
+const nested = require( 'postcss-nested' );
+const postcssFlexbugsFixes = require( 'postcss-flexbugs-fixes' );
 
 module.exports = ( ctx ) => {
 	const config = {
-		plugins,
+		plugins: [
+			atImport(),
+			postcssFlexbugsFixes,
+			nested,
+			customMedia(),
+			minmax(),
+			autoprefixer,
+		],
 	};
 
 	if ( ctx.env === 'production' ) {
