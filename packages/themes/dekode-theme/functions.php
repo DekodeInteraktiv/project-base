9d357a39b950cac98e37f3e92bfb2692155ec893
Petter Walbø Johnsgård <petter@dekode.no>
2021-07-16 13:20:15 +0200

    Cleanup assets loading (#249)
    * Cleanup assets loading
    
    * Update assets.php
    
    * Update assets.php
    
    * Update assets.php
    
    * Update assets.php

--- packages/themes/dekode-theme/functions.php
+++ packages/themes/dekode-theme/functions.php
@@ -7,26 +7,6 @@
 
 declare( strict_types=1 );
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Silence is golden.' );
-}
-
-if ( ! function_exists( 'get_dekode_theme_version' ) ) {
-	/**
-	 * Get the current theme version. Used for asset loading.
-	 *
-	 * @return string Version number or random number
-	 */
-	function get_dekode_theme_version() : string {
-		$theme_data = wp_get_theme( get_template() );
-		$version    = $theme_data->get( 'Version' );
-
-		return $version ?: (string) wp_rand();
-	}
-}
-
-define( 'DEKODE_THEME_VERSION', get_dekode_theme_version() );
-
 require __DIR__ . '/inc/assets.php';
 require __DIR__ . '/inc/setup.php';
 require __DIR__ . '/inc/setup-header.php';
--- packages/themes/dekode-theme/inc/assets.php
+++ packages/themes/dekode-theme/inc/assets.php
@@ -8,76 +8,35 @@
 declare( strict_types=1 );
 namespace Dekode\Assets;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Silence is golden.' );
-}
-
 /**
- * Get uri to asset file
+ * Enqueue scripts and styles
  *
- * @param  string $file Filename.
- * @return string
+ * @return void
  */
-function get_file_uri( string $file ): string {
-	return get_template_directory_uri() . '/build/' . $file;
-}
-
-/**
- * File file path
- *
- * @param  string $file Filename.
- * @return string
- */
-function get_file_path( string $file ): string {
-	return get_template_directory() . '/build/' . $file;
-}
-
-
-/**
- * Enqueue scripts and styles for the client.
- */
-function scripts_and_styles() {
+function scripts_and_styles() : void {
 	/**
 	 * Stylesheets
 	 */
-	$style_path = get_file_path( 'style.css' );
-	if ( file_exists( $style_path ) ) {
-		wp_enqueue_style(
-			'dekode',
-			get_file_uri( 'style.css' ),
-			[],
-			filemtime( $style_path )
-		);
+	$style_file_path = \get_template_directory() . '/build/style.css';
+	if ( file_exists( $style_file_path ) ) {
+		\wp_enqueue_style( 'dekode-theme-style', \get_template_directory_uri() . '/build/style.css', [], wp_get_theme()->get( 'Version' ) );
 	}
 
 	/**
 	 * JavaScripts
 	 */
-	$index_js_path = get_file_path( 'index.js' );
-	if ( file_exists( $index_js_path ) ) {
-		$script_deps_path    = get_file_path( 'index.asset.php' );
-		$script_dependencies = file_exists( $script_deps_path ) ?
-			require $script_deps_path :
-			[
-				'dependencies' => [],
-				'version'      => filemtime( $index_js_path ),
-			];
-
-		wp_enqueue_script(
-			'dekode',
-			get_file_uri( 'index.js' ),
-			$script_dependencies['dependencies'],
-			$script_dependencies['version'],
-			true
-		);
+	$js_dependencies_file_path = \get_template_directory() . '/build/index.asset.php';
+	if ( file_exists( $js_dependencies_file_path ) ) {
+		$dependencies = require $js_dependencies_file_path;
+		\wp_enqueue_script( 'dekode-theme-script', \get_template_directory_uri() . '/build/index.js', $dependencies['dependencies'], $dependencies['version'], true );
 	}
 }
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\scripts_and_styles' );
+\add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\scripts_and_styles' );
 
 /**
  * Enqueue editor style for the WordPress editor.
  */
 function editor() {
-	wp_enqueue_style( 'dekode-editor', get_template_directory_uri() . '/build/editor.css', [], DEKODE_THEME_VERSION );
+	wp_enqueue_style( 'dekode-theme-editor', get_template_directory_uri() . '/build/editor.css', [], wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\editor' );
