b260f4bb4c8600d38f0e6ea309482949fd5e9348
olethomas <olethomas@dekode.no>
2021-07-16 14:00:15 +0200

    Remove editor styles. Use theme.json

--- packages/themes/dekode-theme/inc/assets.php
+++ packages/themes/dekode-theme/inc/assets.php
@@ -12,7 +12,6 @@ namespace Dekode\Assets;
  * Hooks
  */
 \add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\scripts_and_styles' );
-\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\editor' );
 
 /**
  * Enqueue scripts and styles
@@ -36,11 +35,4 @@ function scripts_and_styles() : void {
 		$dependencies = require $js_dependencies_file_path;
 		\wp_enqueue_script( 'dekode-theme-script', \get_template_directory_uri() . '/build/index.js', $dependencies['dependencies'], $dependencies['version'], true );
 	}
-}
-
-/**
- * Enqueue editor style for the WordPress editor.
- */
-function editor() {
-	\wp_enqueue_style( 'dekode-theme-editor', \get_template_directory_uri() . '/build/editor.css', [], \wp_get_theme()->get( 'Version' ) );
 }
