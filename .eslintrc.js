b48dd907eac84163358933ed8512d5f3d6432c5f
Petter Walbø Johnsgård <petter@dekode.no>
2021-09-08 14:34:51 +0200

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,12 @@
 module.exports = {
-	extends: [ 'plugin:@dekode/base', 'plugin:@dekode/react' ],
-	plugins: [ '@dekode', '@wordpress' ],
-	settings: {
-		react: {
-			version: '16.9.0',
-		},
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	globals: {
+		wp: 'off',
 	},
 	rules: {
+		'jsdoc/require-param': 'off',
+		'@wordpress/no-global-event-listener': 'off',
+		'@wordpress/dependency-group': 'error',
 		'@wordpress/no-unsafe-wp-apis': 'error',
-		'react/react-in-jsx-scope': 'off',
-		'react/jsx-pascal-case': 'off',
-		'react/jsx-props-no-spreading': 'off',
-		'no-undefined': 'off',
-		'import/no-unresolved': [
-			'error',
-			{
-				ignore: [ '@wordpress' ],
-			},
-		],
-		'jsx-a11y/media-has-caption': 'off',
 	},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies, @wordpress/dependency-group */
+
 /**
  * External dependencies
  */
