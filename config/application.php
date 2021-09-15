76015e34bad75af0f8863f55a27cf015fcdb4b3a
Petter Walbø Johnsgård <petter@dekode.no>
2021-09-15 11:21:31 +0200

    Add T2 (#282)
    * Add T2
    
    * Fix
    
    * Update mu loader
    
    * Sort packages
    
    Co-authored-by: Stian <stian@dekode.no>

--- config/application.php
+++ config/application.php
@@ -45,7 +45,9 @@ $dotenv->load( $root_dir . '/.env' );
  *
  * Default: production
  */
-define( 'WP_ENVIRONMENT_TYPE', env( 'WP_ENVIRONMENT_TYPE' ) ?: 'production' );
+if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) {
+	define( 'WP_ENVIRONMENT_TYPE', env( 'WP_ENVIRONMENT_TYPE' ) ?: 'production' );
+}
 
 /**
  * Load enviroment config
