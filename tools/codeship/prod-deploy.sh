fa01f9522fc81eae3e41af85de0c1687c3fd9770
André Ø. Langvand <andre@dekode.no>
2021-09-21 13:59:11 +0200

    Exclude .well-known

--- tools/codeship/prod-deploy.sh
+++ tools/codeship/prod-deploy.sh
@@ -3,4 +3,4 @@
 cd ~/clone
 rm -rf tools
 
-#rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ customer_name@x.prod.dekodes.no:/customer_name/destination
+#rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' --exclude='.well-known' -e "ssh" ~/clone/ customer_name@x.prod.dekodes.no:/customer_name/destination
--- tools/codeship/stage-deploy.sh
+++ tools/codeship/stage-deploy.sh
@@ -6,5 +6,5 @@ cp ~/clone/tools/codeship/post-deploy.sh post-deploy.sh
 
 rm -rf tools
 
-rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' -e "ssh" ~/clone/ customer_name@x.dev05.dekodes.no:/customer_name/destination
+rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' --exclude='.well-known' -e "ssh" ~/clone/ customer_name@x.dev05.dekodes.no:/customer_name/destination
 ssh customer_name@x.dev05.dekodes.no 'bash -s' < ~/clone/post-deploy.sh
