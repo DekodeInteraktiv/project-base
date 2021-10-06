0f90ad2e53936c63b149f73afd63e0b39a86b04d
fnpen <ilya@fnpen.com>
2021-10-06 06:27:26 +0300

    Use multi config setup, support rel paths for SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,116 +3,246 @@
 /**
  * External dependencies
  */
-const glob = require( 'fast-glob' );
-const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
-const fs = require( 'fs' );
-const path = require( 'path' );
-const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
-const IgnoreEmitPlugin = require( 'ignore-emit-webpack-plugin' );
-require( 'dotenv' ).config();
+ const { sync: globSync } = require('fast-glob');
+ const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+ const path = require('path');
+ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+ const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+ const webpack = require('webpack');
+ const { Table } = require('console-table-printer');
+ require('dotenv').config();
 
-/**
- * WordPress dependencies
- */
-const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
-const defaultConfig = require( './node_modules/@wordpress/scripts/config/webpack.config' );
+ /**
+  * WordPress dependencies
+  */
+ const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+ const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');
 
-function getBuildPath( name ) {
-	return `${ name.split( '|' )[ 0 ] }/build/${ name.split( '|' )[ 1 ] }`;
-}
+ /**
+  * Patches config to use resolve-url-loader for relative paths in SCSS files
+  * It will be possible to use './images/png.png' inside the SCSS,
+  *
+  * All paths will be threated as relative to file, not project root.
+  */
+ for (const rule of defaultConfig.module.rules) {
+	 if ('any-filename-to-test.scss'.match(rule.test)) {
+		 const sassLoaderConfig = rule.use.pop();
 
-async function getEntries() {
-	const entries = {};
+		 // We mutate default config to change behaviour.
+		 rule.use = [
+			 ...rule.use,
+			 // resolve-url-loader should be executed right after sass-loader.
+			 {
+				 loader: require.resolve('resolve-url-loader'),
+				 options: {
+					 sourceMap: sassLoaderConfig.options.sourceMap,
+					 removeCR: true,
+				 },
+			 },
+			 {
+				 ...sassLoaderConfig,
+				 options: {
+					 ...sassLoaderConfig.options,
+					 sourceMap: true, // It's required to have sourceMap for resolve-url-loader, resolve-url-loader will keep or drop maps on it's step.
+				 },
+			 },
+		 ];
+	 }
+ }
 
-	let dirs = await glob(
-		[
-			'./packages/mu-plugins/*',
-			'./packages/plugins/*',
-			'./packages/themes/*',
-		],
-		{ onlyDirectories: true }
-	);
+ /**
+  * Collect information about all packages
+  * and their entry files using entry-files.json files.
+  *
+  * @return {Object[]} Array of items with information
+  */
+ function getEntryFiles() {
+	 const entries = [];
 
-	// Only include directories that contains a entry-files.json file.
-	dirs = dirs.filter( ( dir ) =>
-		fs.existsSync( path.resolve( dir, 'entry-files.json' ) )
-	);
+	 // Only include directories that contains a entry-files.json file.
+	 const files = globSync(
+		 './packages/(mu-plugins|plugins|themes)/**/entry-files.json',
+		 { onlyFiles: true }
+	 );
 
-	dirs.forEach( ( dir ) => {
-		const entryFiles = require( `${ dir }/entry-files.json` ); // eslint-disable-line
+	 files.forEach((file) => {
+		 const entryFiles = require( file ); // eslint-disable-line
 
-		entryFiles.forEach( ( entry ) => {
-			/**
-			 * MiniCSSExtractPlugin handles css files. Rename them and ignore in
-			 * IgnoreEmitPlugin to allow as direct entry files.
-			 */
-			entries[
-				`${ dir }|${ entry.replace( '.css', '.css-entry-file' ) }`
-			] = `${ dir }/src/${ entry }`;
-		} );
-	} );
+		 entries.push({
+			 dir: path.dirname(file),
+			 files: entryFiles.map((entryFile) => `src/${entryFile}`),
+		 });
+	 });
 
-	return entries;
-}
+	 return entries;
+ }
 
-/**
- * Config
- */
-const config = {
-	...defaultConfig,
-	entry: async () => getEntries(),
-	output: {
-		path: process.cwd(),
-		filename: ( { chunk } ) => getBuildPath( chunk.name ),
-	},
-	optimization: {
-		...defaultConfig.optimization,
-		splitChunks: {
-			cacheGroups: {
-				style: {
-					type: 'css/mini-extract',
-					test: /[\\/]style(\.module)?\.(sc|sa|c)ss$/,
-					chunks: 'all',
-					enforce: true,
-					name( _, chunks ) {
-						return getBuildPath(
-							chunks[ 0 ].name.replace(
-								'.css-entry-file',
-								'.css'
-							)
-						);
-					},
-				},
-				default: false,
-			},
-		},
-	},
-	resolve: {
-		...defaultConfig.resolve,
-		alias: {
-			...defaultConfig.resolve.alias,
-			components: path.resolve( __dirname, 'packages', 'components' ),
-		},
-	},
-	plugins: [
-		new MiniCSSExtractPlugin( { filename: '[name]' } ),
-		new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
-		new IgnoreEmitPlugin( /\.css-entry-file$/ ),
-	],
-};
+ /**
+  * Closure function to pretty print information about packages and
+  * their entry files of assets.
+  */
+ const packagesTable = (() => {
+	 const entriesForTable = {};
 
-/**
- * Browsersync
- */
-if ( 'true' === process.env.BROWSER_SYNC_ENABLE ) {
-	config.plugins.push(
-		new BrowserSyncPlugin( {
-			files: '**/*.php',
-			proxy: process.env.BROWSER_SYNC_PROXY ?? process.env.WP_HOME,
-			port: process.env.BROWSER_SYNC_PORT ?? 3002,
-			https: 'true' === process.env.BROWSER_SYNC_HTTPS,
-		} )
-	);
-}
+	 const p = new Table({
+		 columns: [
+			 { name: 'dir', title: 'Package', alignment: 'left' },
+			 { name: 'files', title: 'Entry Files', alignment: 'left' },
+		 ],
+	 });
 
-module.exports = config;
+	 return {
+		 printTable: () => {
+			 const dirs = Object.keys(entriesForTable);
+			 dirs.forEach((dir, i) => {
+				 const entryForTable = entriesForTable[dir];
+				 Object.values(entryForTable).forEach((entryForTableItem, j) => {
+					 p.addRow({
+						 dir: j === 0 ? dir : '',
+						 files: entryForTableItem.join(' + '),
+					 });
+				 });
+
+				 if (dirs.length - 1 > i) {
+					 p.addRow({
+						 dir: '───────────────────────────────────────────────────',
+						 files: '────────────────────────────────',
+					 });
+				 }
+			 });
+
+			 p.printTable();
+		 },
+
+		 logEntry: (dir, file) => {
+			 const fileName = path.parse(file).name;
+
+			 if (typeof entriesForTable[dir] === 'undefined') {
+				 entriesForTable[dir] = [];
+			 }
+
+			 if (typeof entriesForTable[dir][fileName] === 'undefined') {
+				 entriesForTable[dir][fileName] = [];
+			 }
+			 entriesForTable[dir][fileName].push(file);
+		 },
+	 };
+ })();
+
+ /**
+  * Prepares Config for defined package and entry files,
+  *
+  * Future: We can differ configuration between packages or use their own configuration
+  * or implement importing webpack.config.js from their folders.
+  *
+  * @param {string} dir   Package full path.
+  * @param {Array}  files Relative paths of enries.
+  * @return {webpack.Configuration} Return single webpack configuration.
+  */
+ const prepareConfig = (dir, files) => {
+	 const entries = {};
+
+	 files.forEach((file) => {
+		 const filePath = path.resolve(__dirname, dir, file);
+		 const fileName = path.parse(file).name;
+
+		 if (typeof entries[fileName] === 'undefined') {
+			 entries[fileName] = [];
+		 }
+
+		 entries[fileName].push(filePath);
+		 packagesTable.logEntry(dir, file);
+	 });
+
+	 const config = {
+		 ...defaultConfig,
+
+		 name: dir,
+		 entry: entries,
+
+		 /**
+		  * It exclude including to bundle external copy of libraries.
+		  */
+		 externals: {
+			 jquery: 'jQuery',
+		 },
+
+		 output: {
+			 path: path.resolve(__dirname, dir, 'build'),
+			 filename: '[name].js',
+		 },
+		 optimization: {
+			 ...defaultConfig.optimization,
+			 removeEmptyChunks: true,
+
+			 splitChunks: {
+				 cacheGroups: {
+					 internalStyle: {
+						 type: 'css/mini-extract',
+						 test: /[\\/]+?\.(sc|sa|c)ss$/,
+						 chunks: 'all',
+						 enforce: true,
+					 },
+					 default: false,
+				 },
+			 },
+		 },
+
+		 resolve: {
+			 ...defaultConfig.resolve,
+			 alias: {
+				 ...defaultConfig.resolve.alias,
+				 components: path.resolve(__dirname, 'packages', 'components'),
+			 },
+		 },
+
+		 // Hides rarely used information for more compact appearance of console.
+		 stats: {
+			 children: false,
+			 all: false,
+			 entrypoints: true,
+			 warnings: true,
+			 errors: true,
+			 hash: false,
+			 timings: true,
+			 errorDetails: true,
+			 builtAt: true,
+		 },
+
+		 plugins: [
+			 new MiniCSSExtractPlugin({ filename: '[name].css' }),
+
+			 /**
+			  * It removes empty JS files, when we use CSS/SCSS as main entrypoint of asset.
+			  *
+			  * It's possible to remove also `.asset.php` by writing custom version
+			  */
+			 new RemoveEmptyScriptsPlugin(),
+
+			 new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
+			 new webpack.ProgressPlugin(),
+
+			 true === process.env.BROWSER_SYNC_ENABLE &&
+				 new BrowserSyncPlugin({
+					 files: '**/*.php',
+					 proxy:
+						 process.env.BROWSER_SYNC_PROXY ?? process.env.WP_HOME,
+					 port: process.env.BROWSER_SYNC_PORT ?? 3002,
+					 https: 'true' === process.env.BROWSER_SYNC_HTTPS,
+				 }),
+		 ].filter(Boolean),
+	 };
+
+	 return config;
+ };
+
+ const files = getEntryFiles();
+
+ /**
+  * We can use Multi-Config mode to build packages in 'sandboxed' mode and parallel.
+  *
+  * https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations
+  */
+ module.exports = files.map((item) => prepareConfig(item.dir, item.files));
+
+ packagesTable.printTable();
