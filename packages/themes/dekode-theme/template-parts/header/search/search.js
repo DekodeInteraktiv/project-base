69f29084efc17df8b3862e29b838c7554cdbbc6f
fnpen <ilya@fnpen.com>
2021-10-06 06:41:11 +0300

    Add prettier to local modules, fix prettier warns

--- packages/themes/dekode-theme/template-parts/header/search/search.js
+++ packages/themes/dekode-theme/template-parts/header/search/search.js
@@ -1,62 +1,62 @@
 /* eslint-disable no-use-before-define */
-const toggleButton = document.querySelector( '.site-header--search--toggle' );
-const formContainer = document.getElementById( 'site-search' );
-const input = document.querySelector( '.site-search .search-field' );
+const toggleButton = document.querySelector('.site-header--search--toggle');
+const formContainer = document.getElementById('site-search');
+const input = document.querySelector('.site-search .search-field');
 
 function hide() {
-	toggleButton.setAttribute( 'aria-expanded', 'false' );
-	formContainer.setAttribute( 'aria-hidden', 'true' );
+	toggleButton.setAttribute('aria-expanded', 'false');
+	formContainer.setAttribute('aria-hidden', 'true');
 
-	document.body.classList.remove( 'site-search__is-visible' );
+	document.body.classList.remove('site-search__is-visible');
 
-	document.removeEventListener( 'mouseup', onMouseUp, {
+	document.removeEventListener('mouseup', onMouseUp, {
 		capture: true,
-	} );
+	});
 
-	document.removeEventListener( 'keydown', handleKeyDown );
+	document.removeEventListener('keydown', handleKeyDown);
 }
 
-function handleKeyDown( event ) {
+function handleKeyDown(event) {
 	const { keyCode } = event;
 
-	if ( keyCode === 27 ) {
+	if (keyCode === 27) {
 		hide();
 	}
 }
 
-function onMouseUp( event ) {
-	const isDescendantOfForm = formContainer.contains( event.target );
-	const isDescendantOfButton = toggleButton.contains( event.target );
+function onMouseUp(event) {
+	const isDescendantOfForm = formContainer.contains(event.target);
+	const isDescendantOfButton = toggleButton.contains(event.target);
 
-	if ( ! isDescendantOfForm && ! isDescendantOfButton ) {
+	if (!isDescendantOfForm && !isDescendantOfButton) {
 		hide();
 	}
 }
 
 function show() {
-	toggleButton.setAttribute( 'aria-expanded', 'true' );
-	formContainer.setAttribute( 'aria-hidden', 'false' );
+	toggleButton.setAttribute('aria-expanded', 'true');
+	formContainer.setAttribute('aria-hidden', 'false');
 
-	document.body.classList.add( 'site-search__is-visible' );
+	document.body.classList.add('site-search__is-visible');
 
-	document.addEventListener( 'mouseup', onMouseUp, {
+	document.addEventListener('mouseup', onMouseUp, {
 		capture: true,
-	} );
+	});
 
-	document.addEventListener( 'keydown', handleKeyDown );
+	document.addEventListener('keydown', handleKeyDown);
 
 	input.focus();
 	input.select();
 }
 
 function toggle() {
-	if ( ! document.body.classList.contains( 'site-search__is-visible' ) ) {
+	if (!document.body.classList.contains('site-search__is-visible')) {
 		show();
 	} else {
 		hide();
 	}
 }
 
-if ( toggleButton ) {
-	toggleButton.addEventListener( 'click', toggle );
+if (toggleButton) {
+	toggleButton.addEventListener('click', toggle);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,246 +3,246 @@
 /**
  * External dependencies
  */
- const { sync: globSync } = require('fast-glob');
- const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
- const path = require('path');
- const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
- const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
- const webpack = require('webpack');
- const { Table } = require('console-table-printer');
- require('dotenv').config();
+const { sync: globSync } = require('fast-glob');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const webpack = require('webpack');
+const { Table } = require('console-table-printer');
+require('dotenv').config();
 
- /**
-  * WordPress dependencies
-  */
- const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
- const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');
+/**
+ * WordPress dependencies
+ */
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');
 
- /**
-  * Patches config to use resolve-url-loader for relative paths in SCSS files
-  * It will be possible to use './images/png.png' inside the SCSS,
-  *
-  * All paths will be threated as relative to file, not project root.
-  */
- for (const rule of defaultConfig.module.rules) {
-	 if ('any-filename-to-test.scss'.match(rule.test)) {
-		 const sassLoaderConfig = rule.use.pop();
+/**
+ * Patches config to use resolve-url-loader for relative paths in SCSS files
+ * It will be possible to use './images/png.png' inside the SCSS,
+ *
+ * All paths will be threated as relative to file, not project root.
+ */
+for (const rule of defaultConfig.module.rules) {
+	if ('any-filename-to-test.scss'.match(rule.test)) {
+		const sassLoaderConfig = rule.use.pop();
 
-		 // We mutate default config to change behaviour.
-		 rule.use = [
-			 ...rule.use,
-			 // resolve-url-loader should be executed right after sass-loader.
-			 {
-				 loader: require.resolve('resolve-url-loader'),
-				 options: {
-					 sourceMap: sassLoaderConfig.options.sourceMap,
-					 removeCR: true,
-				 },
-			 },
-			 {
-				 ...sassLoaderConfig,
-				 options: {
-					 ...sassLoaderConfig.options,
-					 sourceMap: true, // It's required to have sourceMap for resolve-url-loader, resolve-url-loader will keep or drop maps on it's step.
-				 },
-			 },
-		 ];
-	 }
- }
+		// We mutate default config to change behaviour.
+		rule.use = [
+			...rule.use,
+			// resolve-url-loader should be executed right after sass-loader.
+			{
+				loader: require.resolve('resolve-url-loader'),
+				options: {
+					sourceMap: sassLoaderConfig.options.sourceMap,
+					removeCR: true,
+				},
+			},
+			{
+				...sassLoaderConfig,
+				options: {
+					...sassLoaderConfig.options,
+					sourceMap: true, // It's required to have sourceMap for resolve-url-loader, resolve-url-loader will keep or drop maps on it's step.
+				},
+			},
+		];
+	}
+}
 
- /**
-  * Collect information about all packages
-  * and their entry files using entry-files.json files.
-  *
-  * @return {Object[]} Array of items with information
-  */
- function getEntryFiles() {
-	 const entries = [];
+/**
+ * Collect information about all packages
+ * and their entry files using entry-files.json files.
+ *
+ * @return {Object[]} Array of items with information
+ */
+function getEntryFiles() {
+	const entries = [];
 
-	 // Only include directories that contains a entry-files.json file.
-	 const files = globSync(
-		 './packages/(mu-plugins|plugins|themes)/**/entry-files.json',
-		 { onlyFiles: true }
-	 );
+	// Only include directories that contains a entry-files.json file.
+	const files = globSync(
+		'./packages/(mu-plugins|plugins|themes)/**/entry-files.json',
+		{ onlyFiles: true }
+	);
 
-	 files.forEach((file) => {
-		 const entryFiles = require( file ); // eslint-disable-line
+	files.forEach((file) => {
+		const entryFiles = require(file); // eslint-disable-line
 
-		 entries.push({
-			 dir: path.dirname(file),
-			 files: entryFiles.map((entryFile) => `src/${entryFile}`),
-		 });
-	 });
+		entries.push({
+			dir: path.dirname(file),
+			files: entryFiles.map((entryFile) => `src/${entryFile}`),
+		});
+	});
 
-	 return entries;
- }
+	return entries;
+}
 
- /**
-  * Closure function to pretty print information about packages and
-  * their entry files of assets.
-  */
- const packagesTable = (() => {
-	 const entriesForTable = {};
+/**
+ * Closure function to pretty print information about packages and
+ * their entry files of assets.
+ */
+const packagesTable = (() => {
+	const entriesForTable = {};
 
-	 const p = new Table({
-		 columns: [
-			 { name: 'dir', title: 'Package', alignment: 'left' },
-			 { name: 'files', title: 'Entry Files', alignment: 'left' },
-		 ],
-	 });
+	const p = new Table({
+		columns: [
+			{ name: 'dir', title: 'Package', alignment: 'left' },
+			{ name: 'files', title: 'Entry Files', alignment: 'left' },
+		],
+	});
 
-	 return {
-		 printTable: () => {
-			 const dirs = Object.keys(entriesForTable);
-			 dirs.forEach((dir, i) => {
-				 const entryForTable = entriesForTable[dir];
-				 Object.values(entryForTable).forEach((entryForTableItem, j) => {
-					 p.addRow({
-						 dir: j === 0 ? dir : '',
-						 files: entryForTableItem.join(' + '),
-					 });
-				 });
+	return {
+		printTable: () => {
+			const dirs = Object.keys(entriesForTable);
+			dirs.forEach((dir, i) => {
+				const entryForTable = entriesForTable[dir];
+				Object.values(entryForTable).forEach((entryForTableItem, j) => {
+					p.addRow({
+						dir: j === 0 ? dir : '',
+						files: entryForTableItem.join(' + '),
+					});
+				});
 
-				 if (dirs.length - 1 > i) {
-					 p.addRow({
-						 dir: '───────────────────────────────────────────────────',
-						 files: '────────────────────────────────',
-					 });
-				 }
-			 });
+				if (dirs.length - 1 > i) {
+					p.addRow({
+						dir: '───────────────────────────────────────────────────',
+						files: '────────────────────────────────',
+					});
+				}
+			});
 
-			 p.printTable();
-		 },
+			p.printTable();
+		},
 
-		 logEntry: (dir, file) => {
-			 const fileName = path.parse(file).name;
+		logEntry: (dir, file) => {
+			const fileName = path.parse(file).name;
 
-			 if (typeof entriesForTable[dir] === 'undefined') {
-				 entriesForTable[dir] = [];
-			 }
+			if (typeof entriesForTable[dir] === 'undefined') {
+				entriesForTable[dir] = [];
+			}
 
-			 if (typeof entriesForTable[dir][fileName] === 'undefined') {
-				 entriesForTable[dir][fileName] = [];
-			 }
-			 entriesForTable[dir][fileName].push(file);
-		 },
-	 };
- })();
+			if (typeof entriesForTable[dir][fileName] === 'undefined') {
+				entriesForTable[dir][fileName] = [];
+			}
+			entriesForTable[dir][fileName].push(file);
+		},
+	};
+})();
 
- /**
-  * Prepares Config for defined package and entry files,
-  *
-  * Future: We can differ configuration between packages or use their own configuration
-  * or implement importing webpack.config.js from their folders.
-  *
-  * @param {string} dir   Package full path.
-  * @param {Array}  files Relative paths of enries.
-  * @return {webpack.Configuration} Return single webpack configuration.
-  */
- const prepareConfig = (dir, files) => {
-	 const entries = {};
+/**
+ * Prepares Config for defined package and entry files,
+ *
+ * Future: We can differ configuration between packages or use their own configuration
+ * or implement importing webpack.config.js from their folders.
+ *
+ * @param {string} dir   Package full path.
+ * @param {Array}  files Relative paths of enries.
+ * @return {webpack.Configuration} Return single webpack configuration.
+ */
+const prepareConfig = (dir, files) => {
+	const entries = {};
 
-	 files.forEach((file) => {
-		 const filePath = path.resolve(__dirname, dir, file);
-		 const fileName = path.parse(file).name;
+	files.forEach((file) => {
+		const filePath = path.resolve(__dirname, dir, file);
+		const fileName = path.parse(file).name;
 
-		 if (typeof entries[fileName] === 'undefined') {
-			 entries[fileName] = [];
-		 }
+		if (typeof entries[fileName] === 'undefined') {
+			entries[fileName] = [];
+		}
 
-		 entries[fileName].push(filePath);
-		 packagesTable.logEntry(dir, file);
-	 });
+		entries[fileName].push(filePath);
+		packagesTable.logEntry(dir, file);
+	});
 
-	 const config = {
-		 ...defaultConfig,
+	const config = {
+		...defaultConfig,
 
-		 name: dir,
-		 entry: entries,
+		name: dir,
+		entry: entries,
 
-		 /**
-		  * It exclude including to bundle external copy of libraries.
-		  */
-		 externals: {
-			 jquery: 'jQuery',
-		 },
+		/**
+		 * It exclude including to bundle external copy of libraries.
+		 */
+		externals: {
+			jquery: 'jQuery',
+		},
 
-		 output: {
-			 path: path.resolve(__dirname, dir, 'build'),
-			 filename: '[name].js',
-		 },
-		 optimization: {
-			 ...defaultConfig.optimization,
-			 removeEmptyChunks: true,
+		output: {
+			path: path.resolve(__dirname, dir, 'build'),
+			filename: '[name].js',
+		},
+		optimization: {
+			...defaultConfig.optimization,
+			removeEmptyChunks: true,
 
-			 splitChunks: {
-				 cacheGroups: {
-					 internalStyle: {
-						 type: 'css/mini-extract',
-						 test: /[\\/]+?\.(sc|sa|c)ss$/,
-						 chunks: 'all',
-						 enforce: true,
-					 },
-					 default: false,
-				 },
-			 },
-		 },
+			splitChunks: {
+				cacheGroups: {
+					internalStyle: {
+						type: 'css/mini-extract',
+						test: /[\\/]+?\.(sc|sa|c)ss$/,
+						chunks: 'all',
+						enforce: true,
+					},
+					default: false,
+				},
+			},
+		},
 
-		 resolve: {
-			 ...defaultConfig.resolve,
-			 alias: {
-				 ...defaultConfig.resolve.alias,
-				 components: path.resolve(__dirname, 'packages', 'components'),
-			 },
-		 },
+		resolve: {
+			...defaultConfig.resolve,
+			alias: {
+				...defaultConfig.resolve.alias,
+				components: path.resolve(__dirname, 'packages', 'components'),
+			},
+		},
 
-		 // Hides rarely used information for more compact appearance of console.
-		 stats: {
-			 children: false,
-			 all: false,
-			 entrypoints: true,
-			 warnings: true,
-			 errors: true,
-			 hash: false,
-			 timings: true,
-			 errorDetails: true,
-			 builtAt: true,
-		 },
+		// Hides rarely used information for more compact appearance of console.
+		stats: {
+			children: false,
+			all: false,
+			entrypoints: true,
+			warnings: true,
+			errors: true,
+			hash: false,
+			timings: true,
+			errorDetails: true,
+			builtAt: true,
+		},
 
-		 plugins: [
-			 new MiniCSSExtractPlugin({ filename: '[name].css' }),
+		plugins: [
+			new MiniCSSExtractPlugin({ filename: '[name].css' }),
 
-			 /**
-			  * It removes empty JS files, when we use CSS/SCSS as main entrypoint of asset.
-			  *
-			  * It's possible to remove also `.asset.php` by writing custom version
-			  */
-			 new RemoveEmptyScriptsPlugin(),
+			/**
+			 * It removes empty JS files, when we use CSS/SCSS as main entrypoint of asset.
+			 *
+			 * It's possible to remove also `.asset.php` by writing custom version
+			 */
+			new RemoveEmptyScriptsPlugin(),
 
-			 new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
-			 new webpack.ProgressPlugin(),
+			new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
+			new webpack.ProgressPlugin(),
 
-			 true === process.env.BROWSER_SYNC_ENABLE &&
-				 new BrowserSyncPlugin({
-					 files: '**/*.php',
-					 proxy:
-						 process.env.BROWSER_SYNC_PROXY ?? process.env.WP_HOME,
-					 port: process.env.BROWSER_SYNC_PORT ?? 3002,
-					 https: 'true' === process.env.BROWSER_SYNC_HTTPS,
-				 }),
-		 ].filter(Boolean),
-	 };
+			true === process.env.BROWSER_SYNC_ENABLE &&
+				new BrowserSyncPlugin({
+					files: '**/*.php',
+					proxy:
+						process.env.BROWSER_SYNC_PROXY ?? process.env.WP_HOME,
+					port: process.env.BROWSER_SYNC_PORT ?? 3002,
+					https: 'true' === process.env.BROWSER_SYNC_HTTPS,
+				}),
+		].filter(Boolean),
+	};
 
-	 return config;
- };
+	return config;
+};
 
- const files = getEntryFiles();
+const files = getEntryFiles();
 
- /**
-  * We can use Multi-Config mode to build packages in 'sandboxed' mode and parallel.
-  *
-  * https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations
-  */
- module.exports = files.map((item) => prepareConfig(item.dir, item.files));
+/**
+ * We can use Multi-Config mode to build packages in 'sandboxed' mode and parallel.
+ *
+ * https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations
+ */
+module.exports = files.map((item) => prepareConfig(item.dir, item.files));
 
- packagesTable.printTable();
+packagesTable.printTable();
