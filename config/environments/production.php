b8436afbfceaec56f542e21cf025cf0cd1fd0cab
Stian <stian@dekode.no>
2021-10-28 08:18:12 +0200

    No need to repeat default values

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,8 +7,5 @@
 
 declare( strict_types = 1 );
 
-define( 'WP_DEBUG_DISPLAY', false );
-define( 'SCRIPT_DEBUG', false );
-
 // Disable all file modifications including updates and update notifications.
 define( 'DISALLOW_FILE_MODS', true );
