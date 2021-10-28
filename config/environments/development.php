4a67dd974ac38311d78ba07a01b322bda1043cba
Stian <stian@dekode.no>
2021-10-28 08:18:34 +0200

    Disable SCRIPT_DEBUG and SAVEQUERIES by default

--- config/environments/development.php
+++ config/environments/development.php
@@ -7,10 +7,10 @@
 
 declare( strict_types = 1 );
 
-define( 'SAVEQUERIES', true );
 define( 'WP_DEBUG', true );
-define( 'SCRIPT_DEBUG', true );
 define( 'WP_DEBUG_DISPLAY', true );
+# phpcs:ignore define( 'SCRIPT_DEBUG', true );
+# phpcs:ignore define( 'SAVEQUERIES', true );
 
 if ( defined( 'WP_CLI' ) && WP_CLI && env( 'MYSQLI_DEFAULT_SOCKET' ) ) {
 	ini_set( 'mysqli.default_socket', env( 'MYSQLI_DEFAULT_SOCKET' ) ); // phpcs:ignore
