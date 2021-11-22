1e34d275400db9821d38fb535388c6b8d96b3d61
Ilya Fn <ilya@fnpen.com>
2021-11-23 00:10:15 +0300

    #330 Upgrade eslint to 9.3 (#331)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,4 +12,11 @@ module.exports = {
 		'@wordpress/dependency-group': 'error',
 		'@wordpress/no-unsafe-wp-apis': 'error',
 	},
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		babelOptions: {
+			presets: ['@wordpress/babel-preset-default'],
+		},
+		requireConfigFile: false,
+	},
 };
