db7603d9d09dae22976078879baf3cab1e2ba9e9
Stian <stian@dekode.no>
2022-08-17 09:32:49 +0200

    Remove long comments for quicker overview

--- public/wp-config.php
+++ public/wp-config.php
@@ -7,13 +7,13 @@
 
 declare( strict_types = 1 );
 
-/* Application root directory. */
+// Application root directory.
 $app_root = dirname( __DIR__ );
 
-/* Autoload Composer packages. */
+// Autoload Composer packages.
 require_once $app_root . '/vendor/autoload.php';
 
-/* Load environment variables */
+// Load environment variables.
 if ( file_exists( $app_root . '/.env' ) ) {
 	$dotenv = new \Symfony\Component\Dotenv\Dotenv();
 	$dotenv->load( $app_root . '/.env' );
@@ -37,9 +37,7 @@ function env( string $key, $default = '' ) { // phpcs:ignore NeutronStandard.Fun
 	return $value;
 }
 
-/**
- * Conditionally use, or generate, `WP_HOME` and `WP_SITEURL`.
- */
+// Conditionally use, or generate WP_HOME and WP_SITEURL.
 if ( env( 'WP_HOME' ) ) {
 	define( 'WP_HOME', env( 'WP_HOME' ) );
 	define( 'WP_SITEURL', env( 'WP_SITEURL', WP_HOME ) );
@@ -59,43 +57,22 @@ if ( env( 'WP_HOME' ) ) {
 	define( 'WP_SITEURL', WP_HOME );
 }
 
-/**
- * Set custom content directory.
- */
+// Set custom content directory.
 define( 'WP_CONTENT_DIR', $app_root . '/public/content' );
 define( 'WP_CONTENT_URL', WP_HOME . '/content' );
 
-// ** Database settings - You can get this info from your web host ** //
-
-/** Database hostname */
+// Database settings.
 define( 'DB_HOST', env( 'DB_HOST', 'localhost' ) );
-
-/** The name of the database for WordPress */
 define( 'DB_NAME', env( 'DB_NAME', 'local' ) );
-
-/** Database username */
 define( 'DB_USER', env( 'DB_USER', 'root' ) );
-
-/** Database password */
 define( 'DB_PASSWORD', env( 'DB_PASSWORD', 'root' ) );
-
-/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', env( 'DB_CHARSET', 'utf8mb4' ) );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', env( 'DB_COLLATE', '' ) );
 
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// Database table prefix.
+$table_prefix = env( 'DB_PREFIX', 'wp_' ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+
+// Authentication unique keys and salts.
 define( 'AUTH_KEY', env( 'AUTH_KEY' ) );
 define( 'SECURE_AUTH_KEY', env( 'SECURE_AUTH_KEY' ) );
 define( 'LOGGED_IN_KEY', env( 'LOGGED_IN_KEY' ) );
@@ -105,28 +82,7 @@ define( 'SECURE_AUTH_SALT', env( 'SECURE_AUTH_SALT' ) );
 define( 'LOGGED_IN_SALT', env( 'LOGGED_IN_SALT' ) );
 define( 'NONCE_SALT', env( 'NONCE_SALT' ) );
 
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = env( 'DB_PREFIX', 'wp_' ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
+// Debugging mode and environment type.
 define( 'WP_DEBUG', env( 'WP_DEBUG', false ) );
 define( 'WP_DEBUG_DISPLAY', env( 'WP_DEBUG_DISPLAY', false ) );
 define( 'SCRIPT_DEBUG', env( 'SCRIPT_DEBUG', false ) );
@@ -141,14 +97,14 @@ define( 'DISALLOW_FILE_MODS', env( 'DISALLOW_FILE_MODS', true ) );
 // To make WP load each script on the administration page individually; protects against CVE-2018-6389 DoS attacks.
 define( 'CONCATENATE_SCRIPTS', env( 'CONCATENATE_SCRIPTS', false ) );
 
-/* Disable Redis if the environment file decrees it so. */
+// Disable Redis if the environment file decrees it so.
 define( 'WP_REDIS_DISABLED', env( 'WP_REDIS_DISABLED', false ) );
 
 if ( defined( 'WP_CLI' ) && WP_CLI && env( 'MYSQLI_DEFAULT_SOCKET' ) ) {
 	ini_set( 'mysqli.default_socket', env( 'MYSQLI_DEFAULT_SOCKET' ) ); // phpcs:ignore
 }
 
-/* Multisite */
+// Multisite.
 define( 'WP_ALLOW_MULTISITE', env( 'WP_ALLOW_MULTISITE', false ) );
 define( 'MULTISITE', env( 'MULTISITE', false ) );
 
@@ -173,8 +129,7 @@ if ( WP_DEBUG && ! empty( $_SERVER['DOCUMENT_ROOT'] ) ) {
 	}
 }
 
-
-/* Optional proxy settings. */
+// Proxy settings.
 if ( env( 'WP_PROXY_HOST' ) ) {
 	define( 'WP_PROXY_HOST', env( 'WP_PROXY_HOST' ) );
 }
