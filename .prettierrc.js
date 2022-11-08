23325086d639bb83417c8a37f913a1c1913ddd1b
Stian <stian@dekode.no>
2022-11-08 13:28:34 +0100

    Remove default values

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,6 @@
 module.exports = {
 	useTabs: true,
 	tabWidth: 4,
-	printWidth: 80,
 	singleQuote: true,
 	trailingComma: 'all',
-	bracketSpacing: true,
-	bracketSameLine: false,
-	semi: true,
-	arrowParens: 'always',
 };
