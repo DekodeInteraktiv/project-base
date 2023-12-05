267f4d154c02530e3477c17fca85dcb23d88df7a
Petter Walbø Johnsgård <petter@dekode.no>
2023-12-05 21:58:22 +0100

    Remove legacy codeship deployment

--- tools/codeship/prod-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/clone
-rm -rf tools
-
-#rsync -avz --delete --exclude='post-deploy.sh' --exclude='public/content/uploads' --exclude='public/robots.txt' --exclude='public/.htaccess' --exclude='.env' --exclude='.well-known' -e "ssh" ~/clone/ customer_name@x.prod.dekodes.no:/customer_name/destination
