6e11be231c1bfad645fd3ec7676a2cea307695a0
Petter Walbø Johnsgård <petter@dekode.no>
2023-12-18 14:11:50 +0100

    Add theme.json script

---
+++ tools/utils.js
@@ -0,0 +1,34 @@
+/**
+ * External dependencies
+ */
+const fs = require('fs');
+const path = require('path');
+
+function getRoot() {
+	return path.resolve(process.cwd());
+}
+
+/**
+ * Returns true if the given base file name for a file within directory is
+ * itself a directory.
+ *
+ * @param {string} file Entry file.
+ * @param {string} dir  Entry dir.
+ */
+function isDirectory(file, dir) {
+	return fs.lstatSync(path.resolve(dir, file)).isDirectory();
+}
+
+/**
+ * Get dirs within a directory.
+ *
+ * @param {string} dir Directory to get dirs from.
+ */
+function getDirs(dir) {
+	return fs.readdirSync(dir).filter((file) => isDirectory(file, dir));
+}
+
+module.exports = {
+	getDirs,
+	getRoot,
+};
