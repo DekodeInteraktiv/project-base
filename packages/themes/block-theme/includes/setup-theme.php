3cb726caae478b573ecabfd18e33774b65b1b45c
Stian <stian@dekode.no>
2024-07-29 08:34:39 +0200

    Add template block theme (#505)
    * Updated Composer dependencies
    
    * Partial new block theme
    
    * New block theme templates
    
    * Format recently changed files with the correct Prettier setup
    
    * Use default spacing sizes from WP
    
    * Remove project code
    
    * Use T2 blocks for header and footer
    
    * Remove state toggle class
    
    * Rename
    
    * Set allowed text domains
    
    * Update block name
    
    * Setup default block gap
    
    * Clenaup search and index templates
    
    * Cleanup 404 template
    
    * Inform why this is here
    
    * Setup default internal spacing for Group and Columns
    
    * Use default spacing values from WP
    
    * Style core blocks
    
    * Default block styling
    
    * Change hooks intro comment
    
    * Remove project overrides
    
    * Comment setup files
    
    * Lint fixes
    
    * Fix figcaption styling
    
    * Comment
    
    * Cleanup styling for accordion and ingress
    
    * Remove
    
    * Simplified link list styling
    
    * Use 6.6 beta for now
    
    * Use new spacing var
    
    * Set default block gap
    
    * Use normal font for accordion
    
    * Remove wrapper for core/html block, migrated to T2
    
    * Use new ext
    
    * Updates after merge
    
    * Set block gap for buttons
    
    * Change dev to start
    
    * Use default WP font and spacing sizes
    
    * Less default styling
    
    * Set gap for post-template and featured content layout grids
    
    * Remove breadcrumbs block
    
    * Cleaner templates
    
    * Add the none property
    
    * Better file title
    
    * Remove unused override for core/site-logo block
    
    * Rename none to 00 to align array
    
    * Revert
    
    * Revert
    
    * No need for dom ready here
    
    * Use same wp-scripts version across project
    
    * Update to wp-scripts 28.1 with needed changes to webpack config
    
    * Remove custom theme styles for core blocks
    
    * Support Simple Media+Text
    
    * No need to overwrite separator style when wp-block-styles is removed
    
    * Bloat styling
    
    * No need for size with latest T2
    
    * Move ingress to theme.json. (#508)
    
    * Remove turbo cache on clean
    
    * Bloat style, can be added on demand
    
    * Move gap back into CSS, blockGap does not work in combination with custom bock margin v2
    
    * Bump to WP 6.6 RC1
    
    * Move gap for buttons and post template into theme json
    
    * Bump WP to 6.6, T2 to 7.13
    
    * T2 7.13.2
    
    ---------
    
    Co-authored-by: Ole Thomas Tallerås <olethomas@dekode.no>

---
+++ packages/themes/block-theme/includes/setup-theme.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Setup default theme functionality
+ *
+ * @package BlockTheme
+ */
+
+declare( strict_types = 1 );
+
+namespace BlockTheme\SetupTheme;
+
+defined( 'ABSPATH' ) || exit;
+
+// Hooks.
+\add_action( 'after_setup_theme', __NAMESPACE__ . '\\do_after_setup_theme' );
+\add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\do_enqueue_assets' );
+\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\do_enqueue_block_editor_assets' );
+\add_filter( 't2/extension/enqueue_theme_block_styles/deps', __NAMESPACE__ . '\\do_override_t2_enqueue_theme_block_styles_deps', 65 );
+
+/**
+ * Setup navigation menus and theme supports.
+ *
+ * @return void
+ */
+function do_after_setup_theme(): void {
+
+	// Theme text domain.
+	\load_theme_textdomain( 'block-theme', \get_template_directory() . '/languages' );
+
+	// Add theme supports.
+	\add_theme_support( 'responsive-embeds' );
+	\add_theme_support( 'post-thumbnails' );
+	\add_theme_support( 'custom-logo' );
+}
+
+/**
+ * Enqueue frontend scripts and styles.
+ *
+ * @return void
+ */
+function do_enqueue_assets(): void {
+
+	// Bundled stylesheet.
+	if ( \file_exists( \get_template_directory() . '/build/view.css' ) ) {
+		$assets_version = \filemtime( \get_template_directory() . '/build/view.css' );
+
+		$deps = [];
+
+		/* phpcs:disable
+		// Optionally set all WooCommerce styling as dependency.
+		if ( \class_exists( 'WooCommerce' ) ) {
+			// Optionally incluce Woo styling if plugin is active.
+			$deps[] = 'woocommerce-blocktheme';
+			$deps[] = 'woocommerce-general';
+			$deps[] = 'woocommerce-layout';
+		}
+		phpcs:enable
+		*/
+
+		\wp_enqueue_style( 'block-theme', \get_template_directory_uri() . '/build/view.css', $deps, $assets_version );
+	}
+
+	// Bundled scripts.
+	$build_file  = \get_template_directory() . '/build/view.js';
+	$assets_file = \get_template_directory() . '/build/view.asset.php';
+
+	if ( \file_exists( $build_file ) && \file_exists( $assets_file ) ) {
+		$assets = require $assets_file;
+		\wp_enqueue_script( 'block-theme', \get_template_directory_uri() . '/build/view.js', $assets['dependencies'], $assets['version'], true );
+	}
+}
+
+/**
+ * Enqueue editor style for the WordPress editor.
+ *
+ * @return void
+ */
+function do_enqueue_block_editor_assets(): void {
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	// Bundled stylesheet.
+	if ( \file_exists( \get_template_directory() . '/build/editor.css' ) ) {
+		$assets_version = \filemtime( \get_template_directory() . '/build/editor.css' );
+		\wp_enqueue_style( 'block-theme', \get_template_directory_uri() . '/build/editor.css', [ 'global-styles-css-custom-properties' ], $assets_version );
+	}
+
+	// Bundled scripts.
+	$build_file  = \get_template_directory() . '/build/editor.js';
+	$assets_file = \get_template_directory() . '/build/editor.asset.php';
+
+	if ( \file_exists( $build_file ) && \file_exists( $assets_file ) ) {
+		$assets = require $assets_file;
+		\wp_enqueue_script( 'block-theme', \get_template_directory_uri() . '/build/editor.js', $assets['dependencies'], $assets['version'], true );
+
+		\wp_set_script_translations( 'block-theme', 'block-theme', \get_template_directory() . '/languages' );
+		\wp_enqueue_script( 'block-theme' );
+	}
+}
+
+/**
+ * Set theme style as depenency for all block styles to ensure correct loading order.
+ *
+ * @param array $deps Dependencies.
+ * @return array
+ */
+function do_override_t2_enqueue_theme_block_styles_deps( array $deps ): array {
+	$deps[] = 'block-theme';
+	return $deps;
+}
