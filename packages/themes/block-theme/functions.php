115b06f14327fe8aecc5098e6e9503ee82ebbcfb
Stian <stian.overasen@dekode.no>
2024-10-04 18:37:22 +0200

    Remove blueprint plugin, move example block into theme

--- packages/themes/block-theme/functions.php
+++ packages/themes/block-theme/functions.php
@@ -6,6 +6,7 @@
  */
 
 declare( strict_types = 1 );
+
 defined( 'ABSPATH' ) || exit;
 
 // Autoload package Composer dependencies.
@@ -15,3 +16,6 @@ if ( \file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 
 // Require all files in includes folder.
 \array_map( fn( $f ) => require_once $f, \glob( __DIR__ . '/includes/*.php' ) );
+
+// Require all block.php files in build/ folder.
+\array_map( fn( $f ) => require_once $f, \glob( __DIR__ . '/build/*/block.php' ) );
