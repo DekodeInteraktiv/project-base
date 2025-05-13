b6d91215317ba6fbec727bfa58f1769a80789c46
Stian <stian.overasen@dekode.no>
2025-05-13 14:23:50 +0200

    Set correct date and time format on boot

--- tools/local/setup-main-site.sh
+++ tools/local/setup-main-site.sh
@@ -4,6 +4,8 @@ wp option update blogname "Project Base"
 wp option update blogdescription ""
 wp option update permalink_structure "/%postname%/"
 wp option update timezone_string "Europe/Oslo"
+wp option update date_format "j. F Y"
+wp option update time_format "H:i"
 
 wp theme enable block-theme --activate
 
