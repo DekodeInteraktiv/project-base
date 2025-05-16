1cd268e328f0bf8b134fe75e111d7e5c596f056a
Stian <stian.overasen@dekode.no>
2025-05-16 09:27:08 +0200

    fix

--- tools/local/setup-main-site.sh
+++ tools/local/setup-main-site.sh
@@ -6,12 +6,11 @@ wp option update permalink_structure "/%postname%/"
 wp option update timezone_string "Europe/Oslo"
 wp option update date_format "j. F Y"
 wp option update time_format "H:i"
+wp language core install nb_NO
+wp site switch-language nb_NO
 
 wp theme activate block-theme
 #wp theme enable block-theme --activate
-
-wp language core install nb_NO
-wp site switch-language nb_NO
 
 wp plugin activate t2
 
